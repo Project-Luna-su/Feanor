6b38c8d3f6b6b044dfe122b06571c1ca562621a0
Svatoslav Semenov <semenovsvatoslav19@gmail.com>
2024-02-27 02:26:14 +0300

    artist and playlists

--- App/App/urls.py
+++ App/App/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, re_path
-
 from apps import views as app
 
 urlpatterns = [
     path("", app.index, name="home"),
     path('testlog/', app.testlog),
+    path("tst/", app.test),
     path("artist/<str:artist>", app.artist),
+    path("playlist/<str:name>", app.playlist),
     path('integration/', app.integration),
     path("integration/templink/", app.templink)
 ]
